601a65d178dfbdff6c3b3e135a4bc25854ac1c29
bgoldste <bengoldstein9@gmail.com>
2014-12-06 22:18:51 -0800

    deletes

--- moocb/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from djangotoolbox.fields import ListField
-
-# Create your models here.
-class Subscriber(models.Model):
-	email_address = models.EmailField(unique=True)
-
-	def __unicode__(self):
-		return u'%s' % (self.email_address)	
