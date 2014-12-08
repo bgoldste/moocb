144fed26fffec34adb03bb2c1ea47c0d5f5df316
bgoldste <bengoldstein9@gmail.com>
2014-12-07 17:54:03 -0800

    asd

--- moocb/settings.py
+++ moocb/settings.py
@@ -115,10 +115,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-
-#mport dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
-
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
