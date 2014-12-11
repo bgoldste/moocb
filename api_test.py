72f79172908e9f5d972c4b072aaf977ffc09b17e
bgoldste <bengoldstein9@gmail.com>
2014-12-10 22:22:18 -0800

    asd

--- api_test.py
+++ api_test.py
@@ -1,7 +1,7 @@
 import requests, json
 
 #r = requests.post('https://salty-inlet-9116.herokuapp.com/add/?user=5483c08&time=1000000', data={'hi' : 'asda'})
-r = requests.post('https://127.0.0.1:8000/add/?user=1&time=50&goal=1', data={'hi' : 'asda'})
+r = requests.post('https://salty-inlet-9116.herokuapp.com/add/?user=2&time=50&goal=3', data={'hi' : 'asda'})
 
 #and probs do something else with the response....
 print r.text
