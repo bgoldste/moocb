175fca66ee4ebee1ca4d8a3f3b56db5b49fd82f9
bgoldste <bengoldstein9@gmail.com>
2015-01-04 19:23:53 -0800

    deletes

--- api_test.py
+++
@@ -1,7 +0,0 @@
-import requests, json
-
-#r = requests.post('https://salty-inlet-9116.herokuapp.com/add/?user=5483c08&time=1000000', data={'hi' : 'asda'})
-r = requests.post('https://salty-inlet-9116.herokuapp.com/add/?user=2&time=50&goal=3', data={'hi' : 'asda'})
-
-#and probs do something else with the response....
-print r.text
